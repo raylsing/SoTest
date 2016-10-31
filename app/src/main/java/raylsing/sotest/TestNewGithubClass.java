562d94defa04923fa91108cdd325a476a704ed32
raylsing <178159984@qq.com>
2016-10-31 17:27:52 +0800

    new class

---
+++ app/src/main/java/raylsing/sotest/TestNewGithubClass.java
@@ -0,0 +1,18 @@
+package raylsing.sotest;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2016/10/31.
+ */
+
+public class TestNewGithubClass {
+
+    private String TAG = "TestNewGithubClass";
+
+    public void tTest() {
+        Log.e(TAG, "test for it !");
+    }
+
+
+}
